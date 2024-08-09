1f13671428fbf789cc0b8434bea8c1607bf7ec21
Tim-Labahn <tim.labahn@freaknet.de>
2024-08-09 23:33:44 +0200

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+// Define the base path for GitHub Pages
+const base = '/fallout-dnd/';
+
 export default defineConfig({
   plugins: [vue()],
-})
+  base: base, // Ensures assets are loaded correctly
+  build: {
+    outDir: 'dist', // Output directory for build files
+  },
+});
