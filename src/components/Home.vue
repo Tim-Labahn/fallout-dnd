0f8a3f99c3fd3778ddcb8909591c56b56100f24d
Tim-Labahn <tim.labahn@freaknet.de>
2024-08-09 23:59:05 +0200

    typo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@ const title = 'Welcome to the Fallout Pen-and-Paper RPG';
 const subtitle = 'Your Gateway to Post-Apocalyptic Adventures';
 const description = `Dive into the world of Fallout with our comprehensive Pen-and-Paper RPG rulebook. Explore the wasteland, battle foes, and build your character with the S.P.E.C.I.A.L system. Whether you're a seasoned player or new to the game, find everything you need to start your adventure right here.`;
 const features = [
-    'Detailedddddddddd Character Creation System',
+    'Detailed Character Creation System',
     'Comprehensive Rules for Skills and Combat',
     'A Variety of Perks and Species to Choose From',
     'Engaging Quests and Experience Rewards',
