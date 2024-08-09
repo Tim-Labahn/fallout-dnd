306ebd23e8e11014624ab84646c2d5b46b45499d
Tim-Labahn <tim.labahn@freaknet.de>
2024-08-10 01:37:12 +0200

    new build...

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/fallout-dnd/',
   server: {
     port: 8000,
     open: true, // Öffnet automatisch den Browser
